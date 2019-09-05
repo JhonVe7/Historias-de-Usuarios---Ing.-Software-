635c4de42d188513a296e0c47e7664cbdc222dfd
Danny <danilo.castilla.contreras@gmail.com>
2019-09-05 14:33:50 -0500

    Correcion base de datos

--- registro_usuario.sql
+++ registro_usuario.sql
@@ -2,27 +2,28 @@ create database RegistroUseri;
 use RegistroUseri; 
 
 create table usuario(
-     username char(25),
-     contraseña char(25),
+     username varchar(25),
+     contraseña varchar(25),
      email varchar(50),
-     nombre char(10),
-     apellido char(10),
-     rol int(11),
+     nombre varchar(10),
+     apellido varchar(10),
+     rol varchar(25),
      primary key (username)
 );
 
 create table rol(
      id_rol int(11) auto_increment,
      rol varchar(20),
-     primary key (id_rol),
-     foreign key (id_rol) references usuario(rol)
+     primary key (id_rol)
 );
 
 create table Documentos(
     id_documento int(8) auto_increment,
-    nombre char(25),
-    autor char(25),
-    permisos int(11),
-    foreign key (autor) references usuario(username),
-    foreign key (permisos) references usuario(rol)
+    nombre varchar(25),
+    autor varchar(25),
+    permisos varchar(20),
+    primary key (id_documento)
 );
+ALTER TABLE rol ADD FOREIGN KEY (rol) REFERENCES usuario(username);
+ALTER TABLE Documentos ADD FOREIGN KEY (autor) REFERENCES usuario (username);
+ALTER TABLE Documentos ADD FOREIGN KEY (permisos) REFERENCES usuario (username);
