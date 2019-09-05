be8dd9942e61f4ec6e4aef3f258cf7eb38283bbc
sergiocada <33910635+sergiocada@users.noreply.github.com>
2019-09-05 11:44:43 -0500

    Update registro_usuario.sql

--- registro_usuario.sql
+++ registro_usuario.sql
@@ -2,9 +2,8 @@ create database RegistroUseri;
 use RegistroUseri; 
 
 create table usuario(
-
-	 username char(25),
-	 contraseña char(25),
+     username char(25),
+     contraseña char(25),
      email varchar(50),
      nombre char(10),
      apellido char(10),
@@ -13,16 +12,17 @@ create table usuario(
 );
 
 create table rol(
-	 id_rol int(11) auto_increment,
-     email varchar(50),
+     id_rol int(11) auto_increment,
+     rol varchar(20),
      primary key (id_rol),
-	foreign key (id_rol) references usuario(rol)
-
+     foreign key (id_rol) references usuario(rol)
 );
 
 create table Documentos(
-	id_documento int(8) auto_increment,
+    id_documento int(8) auto_increment,
     nombre char(25),
     autor char(25),
-    foreign key (autor) references usuario(username)
+    permisos int(11),
+    foreign key (autor) references usuario(username),
+    foreign key (permisos) references usuario(rol)
 );
