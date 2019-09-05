33e689f8c188dd54b94e5612aadf53135432be92
JhonVe7 <32178848+JhonVe7@users.noreply.github.com>
2019-09-05 15:11:31 -0500

    Update conexion.php

--- Login/conexion.php
+++ Login/conexion.php
@@ -48,11 +48,4 @@
    }
  }
  mysqli_close($conexion);
-
-$Nombre = $_POST('Nombre');
-$Apellido = $_POST('Apellido');
-$Email = $_POST('Email');
-$Username = $_POST('Username');
-$contrasena = $_POST('contraseña');
-
 ?>
