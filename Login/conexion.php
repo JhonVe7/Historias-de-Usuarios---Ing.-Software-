f9b4f34d4082d7b6b142228e7ac30ed0c8c3e6ce
Danny <danilo.castilla.contreras@gmail.com>
2019-09-08 16:06:12 -0500

    Version oficial 1.0

--- Login/conexion.php
+++ Login/conexion.php
@@ -4,12 +4,11 @@ $host_db = "localhost";
 $user_db = "root";
 $pass_db = "";
 $db_name = "ingdatabase";
-$tbl_name1 = "usuario";
+$tbl_name1 = "usuar";
 
 $conexion = new mysqli($host_db, $user_db, $pass_db, $db_name);
 
 if ($conexion->connect_error) {
-
     die("La conexion falló: " . $conexion->connect_error);
 }
 
@@ -21,23 +20,23 @@ $result = $conexion->query($buscarUsuario);
 $count = mysqli_num_rows($result);
 
 if ($count == 1) {
-
     echo "<br />" . "El Nombre de Usuario ya a sido tomado." . "<br />";
     echo "<a href='index.html'>Por favor escoga otro Nombre</a>";
 } else {
 
     $form_pass = $_POST['Contraseña'];
     $hash = password_hash($form_pass, PASSWORD_BCRYPT);
-    $query = "INSERT INTO usuario (username, contraseña)
-    VALUES ('$_POST[username]', '$hash')";
+    $query = "INSERT INTO usuar VALUES (NULL,'$_POST[Username]','$_POST[Contraseña]','$_POST[Email]','$_POST[Nombre]','$_POST[Apellido]',3)";
 }
 
 if ($conexion->query($query) === TRUE) {
     echo "<br />" . "<h2>" . "Usuario Creado Exitosamente!" . "</h2>";
-    echo "<h4>" . "Bienvenido: " . $_POST['username'] . "</h4>" . "\n\n";
-    echo "<h5>" . "Hacer Login: " . "<a href='login.html'>Login</a>" . "</h5>";
+    echo "<h4>" . "Bienvenido: " . $_POST['Username'] . "</h4>" . "\n\n";
+    echo "<h5>" . "Pasar a la plataforma: " . "<a href='./usuarios.html'>Usuario</a>" . "</h5>";
 } else {
     echo "Error al crear el usuario." . $query . "<br>" . $conexion->error;
 }
 
 mysqli_close($conexion);
+
+?>
--- registro_usuario.sql
+++ registro_usuario.sql
@@ -2,26 +2,19 @@ create database ingdatabase;
 use ingdatabase; 
 
 create table rol(
-	 id_rol int(11) auto_increment,
-	 rol varchar(20),
-     primary key (id_rol)
+	id_rol int(11) auto_increment,
+	rol varchar(20),
+	primary key (id_rol)
 );
 
-create table usuario(
-	 id_user int(11) auto_increment,	
-	 username char(25),
-	 contraseña char(25),
-     email varchar(50),
-     nombre char(10),
-     apellido char(10),
-     id_rol int(11),
-     primary key (id_user),
-     foreign key (id_user) references rol(id_rol)
-);
-
-create table Documentos(
-    id_documento int(11) auto_increment,
-    nombre char(25),
-    autor char(25),
-    foreign key (id_documento) references usuario(id_user)
+create table usuar(
+	id_user int(11) auto_increment,	
+	username char(25),
+	contraseña char(25),
+	email varchar(50),
+	nombre char(10),
+	apellido char(10),
+	id_rol int(11),
+	primary key (id_user),
+	foreign key (id_rol) references rol(id_rol)
 );
