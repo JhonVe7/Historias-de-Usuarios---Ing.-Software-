62814286e2c95ec63b468a3d027ea8157580ad0d
JhonVe7 <32178848+JhonVe7@users.noreply.github.com>
2019-09-09 16:54:21 -0500

    Update ValidarLogin.php

--- Login/ValidarLogin.php
+++ Login/ValidarLogin.php
@@ -1,6 +1,28 @@
 <?php
 
-echo $Username = $_POST['Username'];
-echo $password = $_POST['Password'];
+	include_once 'conexion.php';
 
+	$username = $_POST['Username'];
+	$password = $_POST['contraseña'];
+
+	//comparar sentencia 
+	$sentenciaSQL = "SELECT * FROM usuar where username ='$usuario' AND contraseña ='$password' ";
+
+	//import method 
+	$resultados = $conexion ->query($sentenciaSQL);
+
+		if($resultados->num_rows>0){
+			while($registros = $resultados->fetch_array()){
+				echo $registros['nombre']. <br/>;
+				echo $registros['apellido']. <br/>;
+				echo $registros['email']. <br/>;
+				echo $registros['username']. <br/>;
+				echo $registros['nombre']. <br/>;
+				echo $registros['id_user']. <br/>;
+
+			}
+		}else{
+
+			echo "Datos incorrecto, verifique los datos e intente nuevamente. ";
+		}
 ?>
