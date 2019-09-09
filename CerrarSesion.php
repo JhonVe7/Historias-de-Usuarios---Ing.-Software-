e8e51a5ec31ae5bc40cd117886ee66a9b70f0ea1
JhonVe7 <jvelascoriv@uniminuto.edu.co>
2019-09-09 18:18:12 -0500

    Inicio & Cierre

--- CerrarSesion.php
+++ CerrarSesion.php
@@ -1,4 +1,12 @@
 <?php
+session_start();
+error_reporting(0);
+	$varSesion = $_SESSION['nombreSes'];
 
-
+	if($varSesion == null || $varSesion == ''){
+		echo "error 404, por favor hablar con su administrador";
+		die();
+	} 
+session_destroy();
+header('location: logeo.html');
 ?>
