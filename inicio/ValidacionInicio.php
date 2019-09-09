daddc3b6448358b3ba71b5bd64f2297cc043eea2
JhonVe7 <jvelascoriv@uniminuto.edu.co>
2019-09-09 18:05:32 -0500

    Inicio & Cierre

--- inicio/ValidacionInicio.php
+++ inicio/ValidacionInicio.php
@@ -1,6 +1,33 @@
 <?php
-session_start();
+	
+	session_start();
+	error_reporting(0);
+	$varSesion = $_SESSION['nombreSes'];
 
-echo "bienvenido a su página <strong>".$_SESSION['nombreSes']."</strong>";
+	if($varSesion == null || $varSesion == ''){
+		echo "error 404, por favor hablar con su administrador";
+		die();
+	} 
+
+	
 
 ?>
+<!DOCTYPE html>
+<html lang = "es">
+	<head>
+		<meta charset="utf-8">
+		<title>	Inicio de Sesión</title>
+	</head>
+
+	<body>
+		<?php
+
+		echo "bienvenido a su página <strong>".$_SESSION['nombreSes']."</strong>";
+
+		?>
+		<br></br>
+		
+		<a href="cerrarSesion.php">Salir del Sistema</a>
+	</body>
+
+</html>
