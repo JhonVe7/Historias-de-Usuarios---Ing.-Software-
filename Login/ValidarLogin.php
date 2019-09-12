ff102a9e1021597457400b954c2e6dcd80a7a366
JhonVe7 <32178848+JhonVe7@users.noreply.github.com>
2019-09-12 15:19:27 -0500

    Update ValidarLogin.php

--- Login/ValidarLogin.php
+++ Login/ValidarLogin.php
@@ -3,19 +3,21 @@
 	session_start();
 	include_once 'conexion.php';
 
-	$username = $_POST['Username'];
-	$password = $_POST['Password'];
+	$username = mysqli_real_scape_string($Conexion, $_POST['Username']);
+	$password = mysqli_real_scape_string($Conexion,$_POST['Password']);
+	$passEncrpit = = shal($password);
 
 	//comparar sentencia 
 	$sentenciaSQL = "SELECT * FROM usuar where username ='$username' AND contraseña ='$password' ";
 
 	//import method 
 	$resultados = $conexion->query($sentenciaSQL);
+	$rows = $resultados->num_rows;
 
-		if($resultados->mysql_num_rows()>0){
-
+		if($rows > 0){
+			$row = $resultado -> 
 			while($registros = $resultados->fetch_array()){
-		
+		///
 			$nombre = $registros['nombre'];
 			
 			$_SESSION['nombreSes'] = $nombre;
