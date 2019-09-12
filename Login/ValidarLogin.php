b3a42b6b1d2ed88dc64ea4bf5994cf0df05324d4
JhonVe7 <32178848+JhonVe7@users.noreply.github.com>
2019-09-12 16:19:39 -0500

    Update ValidarLogin.php

--- Login/ValidarLogin.php
+++ Login/ValidarLogin.php
@@ -15,20 +15,22 @@
 	$rows = $resultados->num_rows;
 
 		if($rows > 0){
-			$row = $resultado -> 
+			$row = $resultado ->fetch_assoc();
+			$_SESSION['nombreSes'] = $row['usuar'];	
+			header ('location: inicio/ValidacionInicio.php');
+		/*	
 			while($registros = $resultados->fetch_array()){
-		///
 			$nombre = $registros['nombre'];
-			
 			$_SESSION['nombreSes'] = $nombre;
 			header ('location: inicio/ValidacionInicio.php');
-
 			}
-
+		*/
+			
 		}else{
-
-			echo "Datos incorrecto, verifique los datos e intente nuevamente. ";
-
+			echo "<script>
+				alert('Datos incorrecto, verifique los datos e intente nuevamente.');
+				window.location ='ValidarLogin.php';
+			      </script>";
 		}
-
+		
 ?>
