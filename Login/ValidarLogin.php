e566f127a07e4c0a2d39e6e2c924fe906693d0a5
JhonVe7 <jvelascoriv@uniminuto.edu.co>
2019-09-12 19:43:06 -0500

    valid

--- Login/ValidarLogin.php
+++ Login/ValidarLogin.php
@@ -2,21 +2,24 @@
 
 	include("conexion.php");
 
-	$username = mysqli_real_scape_string($Conexion, $_POST['Username']);
-	$password = mysqli_real_scape_string($Conexion,$_POST['Password']);
-	$passEncrpit = = shal($password);
+	// define datos de entrada a variables
+	if(!empty($_POST)){
 
-	// consulta asignada a una variable
-	$sentenciaSQL = "SELECT id_user FROM usuar where username ='$username' AND contraseña ='$password' ";
+		$username = mysqli_real_scape_string($Conexion, $_POST['Username']);
+		$password = mysqli_real_scape_string($Conexion,$_POST['Password']);
+		$passEncrpit =  shal($password);
 
-	//import method 
-	$resultados = $conexion->query($sentenciaSQL);
-	$rows = $resultados->num_rows;
+		// consulta asignada a una variable
+		$sentenciaSQL = "SELECT id_user FROM usuar where username ='$username' AND contraseña ='$password' ";
+
+		//import method 
+		$resultados = $conexion->query($sentenciaSQL);
+		$rows = $resultados->num_rows;
 
 		if($rows > 0){
 			$row = $resultado ->fetch_assoc();
-			$_SESSION['nombreSes'] = $row['usuar'];	
-			header ('location: inicio/ValidacionInicio.php');
+			$_SESSION['nombreSesion'] = $row["id_user"];	
+			header ("location: inicio/ValidacionInicio.php");
 		/*	
 			while($registros = $resultados->fetch_array()){
 			$nombre = $registros['nombre'];
@@ -24,12 +27,12 @@
 			header ('location: inicio/ValidacionInicio.php');
 			}
 		*/
-			
+				
 		}else{
 			echo "<script>
-				alert('Datos incorrecto, verifique los datos e intente nuevamente.');
-				window.location ='ValidarLogin.php';
-			      </script>";
-		}
-		
+					alert('Datos incorrecto, verifique los datos e intente nuevamente.');
+					window.location ='ValidarLogin.php';
+				  </script>";
+			}
+	}
 ?>
