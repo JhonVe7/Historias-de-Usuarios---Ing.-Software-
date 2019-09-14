a6376e421b208bf9c64913fbf710e65a581d680b
Laura Santacruz <laurasantacruz43@gmail.com>
2019-09-14 09:32:51 -0500

    .

--- documentos2.sql
+++ documentos2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-09-2019 a las 19:18:22
+-- Tiempo de generación: 14-09-2019 a las 16:31:18
 -- Versión del servidor: 10.1.38-MariaDB
 -- Versión de PHP: 7.3.4
 
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `documentos`
+-- Base de datos: `documentos2`
 --
 
 -- --------------------------------------------------------
@@ -32,15 +32,40 @@ CREATE TABLE `tbl_documentos` (
   `titulo` varchar(45) DEFAULT NULL,
   `tamanio` varchar(45) DEFAULT NULL,
   `tipo` varchar(45) DEFAULT NULL,
-  `nombre` varchar(45) DEFAULT NULL
+  `nombre` varchar(45) DEFAULT NULL,
+  `id_documentos` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `tbl_documentos`
 --
 
-INSERT INTO `tbl_documentos` (`titulo`, `tamanio`, `tipo`, `nombre`) VALUES
-('sddsd', '88497', 'application/pdf', 'Taller_ArquitecturaEmpresarial.pdf');
+INSERT INTO `tbl_documentos` (`titulo`, `tamanio`, `tipo`, `nombre`, `id_documentos`) VALUES
+('HH', '59752', 'application/pdf', 'Taller AE.pdf', 1),
+('hjklÃ±', '174218', 'application/pdf', 'Taller Grupal.pdf', 2),
+('uno', '59752', 'application/pdf', 'Taller AE.pdf', 3),
+('bfgd', '174218', 'application/pdf', 'Taller Grupal.pdf', 4),
+('hhh', '115909', 'application/pdf', 'Taller #1_ Arquitectura.pdf', 5);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `tbl_documentos`
+--
+ALTER TABLE `tbl_documentos`
+  ADD PRIMARY KEY (`id_documentos`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `tbl_documentos`
+--
+ALTER TABLE `tbl_documentos`
+  MODIFY `id_documentos` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
